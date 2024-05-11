e63c238699b3dc1404eb0d93da86f3a2ddff8cce
dyakonov <dyakonovv2004@yandex.ru>
2024-05-11 21:11:33 +0300

    changestructure

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,15 @@
+# Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request, 'exam/login.html')
+
+def my_acc(request):
+    return render(request, 'exam/my_acc.html')
+
+def my_authors(request):
+    return render(request, 'exam/my_authors.html')
+
+def my_tests(request):
+    return render(request, 'exam/my_tests.html')
+
--- exampro/urls.py
+++ exampro/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from exam.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login, name='login'),
+    path('my_acc/', my_acc, name='my_acc'),
+    path('my_authors/', my_authors, name='my_authors'),
+    path('my_tests/', my_tests, name='my_tests'),
 ]
-##
